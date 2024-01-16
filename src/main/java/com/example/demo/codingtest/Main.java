f2a99b1b54b1be59ff2576d7d83f49c52c52b20d
sonny <dhsmf4245@gmail.com>
2024-01-16 11:07:26 +0900

    백준 24263번 풀이

--- src/main/java/com/example/demo/codingtest/Main.java
+++ src/main/java/com/example/demo/codingtest/Main.java
@@ -1,9 +1,13 @@
 package com.example.demo.codingtest;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        System.out.println(n);
         System.out.println(1);
-        System.out.println(0);
     }
 }
