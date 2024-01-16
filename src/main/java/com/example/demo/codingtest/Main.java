a70637b4f77b098378cfb0be16975e9309372ca1
sonny <dhsmf4245@gmail.com>
2024-01-16 16:04:56 +0900

    2864

--- src/main/java/com/example/demo/codingtest/Main.java
+++ src/main/java/com/example/demo/codingtest/Main.java
@@ -2,13 +2,24 @@ package com.example.demo.codingtest;
 
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
 
-        Long n = sc.nextLong();
-        sc.close();
-        System.out.println(n*n);
-        System.out.println(2);
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        String c = String.valueOf(a);
+        String d = String.valueOf(b);
+
+        String minC = c.replace("6","5");
+        String minD = d.replace("6","5");
+        String maxC = c.replace("5","6");
+        String maxD = d.replace("5","6");
+
+        int min = Integer.parseInt(minC) + Integer.parseInt(minD);
+        int max = Integer.parseInt(maxC) + Integer.parseInt(maxD);
+
+        System.out.println(min +" "+ max);
     }
 }
