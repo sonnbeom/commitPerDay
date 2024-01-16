7c04c12248f048212d8cbd60069b8d56d473d686
sonny <dhsmf4245@gmail.com>
2024-01-16 10:56:02 +0900

    백준 24262번 풀이

--- src/main/java/com/example/demo/codingtest/Main.java
+++ src/main/java/com/example/demo/codingtest/Main.java
@@ -1,21 +1,9 @@
 package com.example.demo.codingtest;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-
-import java.io.*;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args){
 
-        int n = Integer.parseInt(br.readLine());
-        br.close();
-
-        // Math의 클래스의 pow()메서드를 사용했다.
-        // Math.pow(거듭제곱할 숫자, 거듭제곱(몇제곱인지);
-        System.out.println((int)Math.pow(Math.pow(2, n) +1, 2));
+        System.out.println(1);
+        System.out.println(0);
     }
 }
