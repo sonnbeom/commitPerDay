2795a5668de38b96bdfe0dedfa9e8f3d5490b3a6
sonny <dhsmf4245@gmail.com>
2024-01-16 14:59:05 +0900

    24264

--- src/main/java/com/example/demo/codingtest/Main.java
+++ src/main/java/com/example/demo/codingtest/Main.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+
+        Long n = sc.nextLong();
         sc.close();
-        System.out.println(n);
-        System.out.println(1);
+        System.out.println(n*n);
+        System.out.println(2);
     }
 }
