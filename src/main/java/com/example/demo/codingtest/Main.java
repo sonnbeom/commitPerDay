7a8a9dadef71eb279bb12346c43986641e8301e0
sonny <dhsmf4245@gmail.com>
2024-01-17 11:32:21 +0900

    2217

--- src/main/java/com/example/demo/codingtest/Main.java
+++ src/main/java/com/example/demo/codingtest/Main.java
@@ -1,25 +1,25 @@
 package com.example.demo.codingtest;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        String c = String.valueOf(a);
-        String d = String.valueOf(b);
-
-        String minC = c.replace("6","5");
-        String minD = d.replace("6","5");
-        String maxC = c.replace("5","6");
-        String maxD = d.replace("5","6");
-
-        int min = Integer.parseInt(minC) + Integer.parseInt(minD);
-        int max = Integer.parseInt(maxC) + Integer.parseInt(maxD);
-
-        System.out.println(min +" "+ max);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());  //수 입력받기
+        }
+        Arrays.sort(arr, Collections.reverseOrder());  //내림차순으로 정렬하기
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            total = Math.max(total, arr[i] * (i+1));
+        }
+        System.out.print(total);
     }
 }
