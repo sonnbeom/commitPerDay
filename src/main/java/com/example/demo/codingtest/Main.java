4a9461410b072bc854c3dd572defbf6dc763816c
sonny <dhsmf4245@gmail.com>
2024-01-19 08:57:17 +0900

    Algorism 1931

--- src/main/java/com/example/demo/codingtest/Main.java
+++ src/main/java/com/example/demo/codingtest/Main.java
@@ -1,37 +1,44 @@
 package com.example.demo.codingtest;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
-import java.util.StringTokenizer;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
-        //내림차순 정렬을 하려면 int가 아닌 Integer로 선언
-        Integer[] B = new Integer[N];
+    public static void main(String[] args){
+        /*
+        * */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] time = new int[n][2];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-
-        for(int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
-            B[i] = Integer.parseInt(st2.nextToken());
+        for (int i = 0; i < n; i++) {
+            time[i][0] = sc.nextInt();
+            time[i][1] = sc.nextInt();
         }
-        //A 올림, B 내림
-        Arrays.sort(A);
-        Arrays.sort(B, Collections.reverseOrder());
+        Arrays.sort(time, new Comparator<int[]>() {
+            // compare 메소드가 양수를 반환화면 두 요소의 위치를 바꾼다
+            //아래 방식은 오름차순 정렬이다 1 2 3.... 이런 식
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1]==o2[1]){
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
+            }
+        });
 
-        int sum = 0;
+        int count = 0;
+        int endTime = 0;
 
-        for(int i = 0; i < N; i++) {
-            sum += (A[i] * B[i]);
+        for (int i = 0; i < n; i++) {
+            if (endTime <= time[i][0]){
+                count++;
+                endTime = time[i][1];
+            }
         }
-        System.out.println(sum);
+        System.out.println(count);
+
     }
 }
