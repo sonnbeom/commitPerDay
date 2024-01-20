eec4a4dbc9ac12d1fc3dccfd9c0854aafbfd55b8
sonny <dhsmf4245@gmail.com>
2024-01-20 11:06:37 +0900

    Algorism 5585

--- src/main/java/com/example/demo/codingtest/Main.java
+++ src/main/java/com/example/demo/codingtest/Main.java
@@ -5,29 +5,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        Long num = sc.nextLong();
-        /*
-        * 1. 4,294,967,295가 최댓값이므로 Long 타입을 써줍니다.
-        * 2. 공차를 1로 작은 숫자부터 더합니다
-        * 3. 주어진 숫자보다 합이 클 경우 멈춥니다.
-        * 4. 한번 더 더해지지 않을 경우 최대 갯수 N개로 자연수의 합S를 구현한 것이므로
-        * 5. count에서 -1를 진행하고 출력합니다.
-        * */
-        Long comNum = 0L;
-        int count = 0;
-        for (int i = 1; ; i++) {
-            if (comNum>num){
-                break;
-            }
-            comNum += i;
-            count++;
+        int taro = sc.nextInt();
+        int price = 1000;
+        int result = 0;
+        int rest = 0;
+        result = price-taro;
+        if (result>=500){
+            rest += result/500;
+            result = result % 500;
+        }if (result>=100){
+            rest += result/100;
+            result = result % 100;
         }
-        System.out.println(count-1);
-        sc.close();
+        if (result>=50){
+            rest += result/50;
+            result = result % 50;
+        }
+        if (result>=10){
+            rest += result/10;
+            result = result % 10;
+        }
+        if (result>=5){
+            rest += result/5;
+            result = result % 5;
+        }
+        if (result>=1){
+            rest += result/1;
+        }
+        System.out.println(rest);
     }
 }
-/*
-* 1234
-* 9
-* 1 2 3 4
-* */
