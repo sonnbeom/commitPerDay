ef6a55fa95ce7e2a7f8fdeb73ed03be37ee2eaf8
sonny <dhsmf4245@gmail.com>
2024-01-22 09:18:56 +0900

    Algorism 10162

--- src/main/java/com/example/demo/codingtest/Main.java
+++ src/main/java/com/example/demo/codingtest/Main.java
@@ -4,6 +4,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int rest = 0;
 
+        if (t%10 != 0){
+            System.out.println(-1);
+        }else {
+            //t가 300이 넘지 않는다면 0이 할당됩니다.
+            a = t/300;
+            //t가 300이 넘지 않는다면 t값이 그대로 t에 할당됩니다.
+            t %= 300;
+            //t가 60이 넘지 않는다면 0이 할당됩니다.
+            b = t/60;
+            //t가 60이 넘지 않는다면 t값이 그대로 t에 할당됩니다.
+            t %= 60;
+            //if문에서 10으로 나누어 떨어지는 정수임을 확인했기에 t에서 10만 나눠줍니다.
+            c = t/10;
+            System.out.println(a+" "+b+" "+c);
+        }
     }
 }
