e9226815d91c5eeac782d252cc5d7081625f8c74
sonny <dhsmf4245@gmail.com>
2024-01-25 10:32:11 +0900

    Algorism 1343

--- src/main/java/com/example/demo/codingtest/Main.java
+++ src/main/java/com/example/demo/codingtest/Main.java
@@ -1,34 +1,35 @@
 package com.example.demo.codingtest;
 
-import java.util.Scanner;
+import java.io.*;
+
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int j = sc.nextInt();
-        //스크린과 바구니를 배열로 생각하기 설명을 위해 적어놓은 것이니 screen과 basket은 지우셔도 됩니다.
-        int[] screen = new int[n];
-        int[] basket = new int[m];
-        int left = 0;
-        int right = left + m -1;
-        // 주어진 사과의 위치(apple)와 바구니의 위치 차이(left 혹은 right)를 출력하기 위해 전역변수 설정
-        int distance = 0;
-        for (int i = 0; i < j; i++) {
-            int apple = sc.nextInt()-1;
-            if (left>apple){
-                int diff = left-apple;
-                left-=diff;
-                right-=diff;
-                distance += diff;
-            } else if (apple>right) {
-                int diff = apple-right;
-                left += diff;
-                right += diff;
-                distance += diff;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String ss = br.readLine();        String[] xs = ss.split("\\.");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < xs.length; i++) {
+            String x = xs[i];
+            if (x.length() % 2 != 0) {
+                sb = new StringBuilder("-1");
+                break;}
+            else if (x.length() % 4 == 0) {
+                sb.append("AAAA".repeat(x.length() / 4));
+            }else {
+                sb.append("AAAA".repeat(x.length() / 4));
+                sb.append("BB");            }
+            if (i != xs.length - 1) {
+                sb.append(".");
             }
         }
-        System.out.println(distance);
+        String ans = sb.toString();
+        if (!ans.equals("-1")) {
+            sb.append(".".repeat(ss.length() - sb.toString().length()));        }
+        bw.write(sb + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+
     }
 }
