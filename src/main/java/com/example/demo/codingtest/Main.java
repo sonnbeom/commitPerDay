4807660ce20f4f7e1f8f8d3db0276a5285281495
sonny <dhsmf4245@gmail.com>
2024-01-26 15:29:28 +0900

    Algorism 9237

--- src/main/java/com/example/demo/codingtest/Main.java
+++ src/main/java/com/example/demo/codingtest/Main.java
@@ -1,35 +1,25 @@
 package com.example.demo.codingtest;
 
-import java.io.*;
-
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String ss = br.readLine();        String[] xs = ss.split("\\.");
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < xs.length; i++) {
-            String x = xs[i];
-            if (x.length() % 2 != 0) {
-                sb = new StringBuilder("-1");
-                break;}
-            else if (x.length() % 4 == 0) {
-                sb.append("AAAA".repeat(x.length() / 4));
-            }else {
-                sb.append("AAAA".repeat(x.length() / 4));
-                sb.append("BB");            }
-            if (i != xs.length - 1) {
-                sb.append(".");
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Integer[] arr = new Integer[n];
+        int min = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr, Comparator.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            int max = arr[i]+i+1;
+            if (max>min){
+                min = max;
             }
         }
-        String ans = sb.toString();
-        if (!ans.equals("-1")) {
-            sb.append(".".repeat(ss.length() - sb.toString().length()));        }
-        bw.write(sb + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
-
+        System.out.println(min+1);
     }
 }
