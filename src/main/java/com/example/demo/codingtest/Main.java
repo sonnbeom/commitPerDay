783fafa0f2187a521d445872e74711a899e0c4d5
sonny <dhsmf4245@gmail.com>
2024-01-27 17:04:01 +0900

    Algorism 1343

--- src/main/java/com/example/demo/codingtest/Main.java
+++ src/main/java/com/example/demo/codingtest/Main.java
@@ -1,35 +1,22 @@
 package com.example.demo.codingtest;
 
-import java.io.*;
-
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String ss = br.readLine();        String[] xs = ss.split("\\.");
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < xs.length; i++) {
-            String x = xs[i];
-            if (x.length() % 2 != 0) {
-                sb = new StringBuilder("-1");
-                break;}
-            else if (x.length() % 4 == 0) {
-                sb.append("AAAA".repeat(x.length() / 4));
-            }else {
-                sb.append("AAAA".repeat(x.length() / 4));
-                sb.append("BB");            }
-            if (i != xs.length - 1) {
-                sb.append(".");
-            }
-        }
-        String ans = sb.toString();
-        if (!ans.equals("-1")) {
-            sb.append(".".repeat(ss.length() - sb.toString().length()));        }
-        bw.write(sb + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        sc.close();
 
+        String A = "AAAA";
+        String B = "BB";
+
+        //
+        s = s.replaceAll("XXXX",A);
+        s = s.replaceAll("XX",B);
+        if (s.contains("X")){
+            s = "-1";
+        }
+        System.out.println(s);
     }
 }
