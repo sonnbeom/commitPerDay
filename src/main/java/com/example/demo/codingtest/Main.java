f55829bded10acbe476b78d27d152d5a683714b2
sonny <dhsmf4245@gmail.com>
2024-01-28 10:47:28 +0900

    Algorism 2212

--- src/main/java/com/example/demo/codingtest/Main.java
+++ src/main/java/com/example/demo/codingtest/Main.java
@@ -1,22 +1,35 @@
 package com.example.demo.codingtest;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        sc.close();
+        int n = sc.nextInt();
+        int k = sc.nextInt();
 
-        String A = "AAAA";
-        String B = "BB";
-
-        //
-        s = s.replaceAll("XXXX",A);
-        s = s.replaceAll("XX",B);
-        if (s.contains("X")){
-            s = "-1";
+        if (k >= n){
+            System.out.println(0);
+            return;
         }
-        System.out.println(s);
+        int sum = 0;
+        int[] sensorArr = new int[n];
+        for (int i = 0; i < n; i++) {
+            sensorArr[i] = sc.nextInt();
+        }
+        Arrays.sort(sensorArr);
+
+        Integer[] diffArr = new Integer[n-1];
+        for (int i = 0; i < n-1; i++) {
+            diffArr[i] = sensorArr[i+1] - sensorArr[i];
+        }
+        Arrays.sort(diffArr, Collections.reverseOrder());
+
+        for (int i = k-1; i < n-1; i++) {
+            sum += diffArr[i];
+        }
+        System.out.println(sum);
     }
 }
