3c6a70285ef2199b6bdb3d56317f51e362f32c7b
sonny <dhsmf4245@gmail.com>
2024-01-29 15:51:06 +0900

    Algorism 2847 / annotation

--- src/main/java/com/example/demo/codingtest/Main.java
+++ src/main/java/com/example/demo/codingtest/Main.java
@@ -1,38 +1,39 @@
 package com.example.demo.codingtest;
 
-import java.util.Arrays;
-import java.util.Collections;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+
         Scanner sc =new Scanner(System.in);
         int n = sc.nextInt();
-        int k = sc.nextInt();
-        if(k >= n) {
-            System.out.println(0);
-            return;
-        }
-        int[] sensorArray = new int[n];
+        int[] levelArr = new int[n];
+
         for (int i = 0; i < n; i++) {
-            sensorArray[i] = sc.nextInt();
+            levelArr[i] = sc.nextInt();
         }
-        // 센서의 거리를 오름차순으로 정렬 -> 센서의 거리 차이 중 가장 큰 값을 알기 위함
-        Arrays.sort(sensorArray);
-        // 센서의 거리 차이만큼 배열에 담아줍니다. 차이가 작은 순으로 정렬되어있습니다.
-        Integer[] diff = new Integer[n-1];
-        for (int i = 0; i < n-1; i++) {
-            diff[i] = sensorArray[i+1] - sensorArray[i];
-        }
-        // 차이 배열을 내림차순으로 정렬합니다.
-        Arrays.sort(diff, Collections.reverseOrder());
+
         int sum = 0;
-        // 집중국의 갯수에 따라 수신 가능 영역 길이의 합이 감소합니다.
-        // 집중국이 2개라면 1개, 집중국이 3개라면 2개가 감소하기 때문에 k-1로 두었습니다.
-        // diff 배열 크기가 n-1이기 때문에 i < n-1로 두었습니다.
-        for (int i = k-1; i < n-1; i++) {
-            sum += diff[i];
+        /* index가 0부터 시작하므로 n-1부터 시작합니다
+        * diff<=0일 때 배열 i번째에서 i-1번째를 빼면 음수가 나옵니다. 이를 Math.abs()을 통해 절댓값으로 감쌉니다.
+        * i-1번째가 i번째보다 최소 1이 작아야하므로 차이에 1을 더해줍니다.
+        * i-1번째 숫자의 차이+1만큼을 빼주어 배열의 오름차순을 만듭니다.
+        * */
+        for (int i = n-1; i >= 1; i--) {
+            int diff = levelArr[i] - levelArr[i-1];
+            if (diff<=0){
+                int num = Math.abs(diff)+1;
+                sum += num;
+                levelArr[i-1] -= num;
+            }
         }
         System.out.println(sum);
     }
 }
+/*
+* 5
+* 5
+* 5
+*
+* */
