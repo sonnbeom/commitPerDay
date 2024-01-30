bf42793991fe41098391414a709441d69bc543cd
sonny <dhsmf4245@gmail.com>
2024-01-30 20:44:45 +0900

    Algorism 1920

--- src/main/java/com/example/demo/codingtest/Main.java
+++ src/main/java/com/example/demo/codingtest/Main.java
@@ -1,47 +1,28 @@
 package com.example.demo.codingtest;
 
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
 
 public class Main {
-    static int[] arr;
-    static boolean[] visit;
-    static StringBuilder st;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int input = sc.nextInt();
+            map.put(input, input);
+        }
         int m = sc.nextInt();
-        arr = new int[m];
-        visit = new boolean[m];
-
-        dfs(n, m, 0);
-
-
-    }
-    private static void dfs(int n, int m, int depth){
-
-        if (depth==m){
-            for(int num: arr){
-                st.append(num).append(" ");
-            }
-            st.append("\n");
-            return;
-        }
-
-        for(int i=0; i<n; i++){
-            if (!visit[i]) {
-                visit[i] = true;
-                arr[depth] = i+1;
-                dfs(n,m,depth+1);
-                visit[i] = false;
-
+        for (int i = 0; i < m; i++) {
+            int input = sc.nextInt();
+            boolean contain =  map.containsKey(input);
+            if (contain){
+                System.out.println(1);
+            }else {
+                System.out.println(0);
             }
         }
     }
-
 }
+
 
