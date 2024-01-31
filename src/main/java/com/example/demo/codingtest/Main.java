e7b16f074f1953d29a155a225d82811c458b6a27
sonny <dhsmf4245@gmail.com>
2024-01-31 15:47:40 +0900

    Algorism 9461

--- src/main/java/com/example/demo/codingtest/Main.java
+++ src/main/java/com/example/demo/codingtest/Main.java
@@ -1,43 +1,38 @@
 package com.example.demo.codingtest;
 
+
 import java.util.Scanner;
 
 public class Main {
-
-    public static int[] arr;
-    public static boolean[] visit;
-
+    static long arr[];
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-        int N = in.nextInt();
-        int M = in.nextInt();
+        arr = new long[101];
+        arr[1] = 1;
+        arr[2] = 1;
+        arr[3] = 1;
 
-        arr = new int[M];
-        visit = new boolean[N];
-        dfs(N, M, 0);
-
-    }
-
-    public static void dfs(int N, int M, int depth) {
-        if (depth == M) {
-            for (int val : arr) {
-                System.out.print(val + " ");
-            }
-            System.out.println();
-            return;
-        }
-
-        for (int i = 0; i < N; i++) {
-            if (!visit[i]) {
-                visit[i] = true;
-                arr[depth] = i + 1;
-                dfs(N, M, depth + 1);
-                visit[i] = false;
+        for (int i = 0; i < n; i++) {
+            int p = sc.nextInt();
+            if (p<4){
+                System.out.println(arr[p]);
+            }else {
+                long ans = pr(p);
+                System.out.println(ans);
             }
         }
     }
-
+    private static long pr(int p) {
+        for (int i = 4; i <= p; i++) {
+            arr[i] = arr[i-2] + arr[i-3];
+        }
+        return arr[p];
+    }
 }
 
+
+
+//   A = An-2  An -3
