41820b8c62137d515de21c6a5534632900c88bac
sonny <dhsmf4245@gmail.com>
2024-01-31 17:27:35 +0900

    Algorism 15650

--- src/main/java/com/example/demo/codingtest/Main.java
+++ src/main/java/com/example/demo/codingtest/Main.java
@@ -4,35 +4,37 @@ package com.example.demo.codingtest;
 import java.util.Scanner;
 
 public class Main {
-    static long arr[];
-    public static void main(String[] args) {
+    static int N;
+    static int M;
 
+    static int[]arr;
+
+    static StringBuilder st = new StringBuilder();
+
+    public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        N = sc.nextInt();
+        M = sc.nextInt();
 
-        arr = new long[101];
-        arr[1] = 1;
-        arr[2] = 1;
-        arr[3] = 1;
-
-        for (int i = 0; i < n; i++) {
-            int p = sc.nextInt();
-            if (p<4){
-                System.out.println(arr[p]);
-            }else {
-                long ans = pr(p);
-                System.out.println(ans);
-            }
-        }
+        arr = new int[M];
+        dfs(1,0);
+        System.out.println(st);
     }
-    private static long pr(int p) {
-        for (int i = 4; i <= p; i++) {
-            arr[i] = arr[i-2] + arr[i-3];
+    static void dfs(int at, int depth){
+
+        if(depth==M){
+            for(int num: arr){
+                st.append(num).append(" ");
+            }
+            st.append("\n");
+            return;
         }
-        return arr[p];
+
+        for(int i = at; i<= N; i++){
+
+            arr[depth] = i;
+            dfs(i+1, depth+1);
+
+        }
     }
 }
-
-
-
-//   A = An-2  An -3
