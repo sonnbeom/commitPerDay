14cba5c556d749c8235bdcd961cc1a23bab87e0e
sonny <dhsmf4245@gmail.com>
2024-02-01 09:09:29 +0900

    algorism 1463

--- src/main/java/com/example/demo/codingtest/Main.java
+++ src/main/java/com/example/demo/codingtest/Main.java
@@ -1,29 +1,33 @@
 package com.example.demo.codingtest;
 
-
-import java.util.Arrays;
 import java.util.Scanner;
 
+
 public class Main {
-    public static void main(String[] args){
+    static Integer[] dp;
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int l = sc.nextInt();
-        int[] arr = new int[n];
+        int N = sc.nextInt();
 
-        for (int i = 0; i <n ; i++) {
-            arr[i] = sc.nextInt();
-        }
-        Arrays.sort(arr);
+        dp = new Integer[N+1];
+        dp[0] = dp[1] = 0;
+        System.out.println(recur(N));
+    }
+    static int recur(int N){
 
-        int count = 0;
-        int num = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (arr[i]>num){
-                num = arr[i]+l-1;
-                count++;
+        if (dp[N]==null){
+            if (N%6==0){
+                dp[N] = Math.min(recur(N-1), Math.min(recur(N/3), recur(N/2))) + 1;
+            } else if (N%3==0) {
+                dp[N] = Math.min(recur(N/3), recur(N-1))+1;
+            } else if (N%2==0) {
+                dp[N] = Math.min(recur(N/2), recur(N-1))+1;
             }
-        }System.out.println(count);
+            else {
+                dp[N] = recur(N-1) +1;
+            }
+        }
+        return dp[N];
     }
 }
