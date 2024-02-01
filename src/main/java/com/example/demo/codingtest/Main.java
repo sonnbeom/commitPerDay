d073fd0b4fe8cc37d0537376f5f7de2aaa65d1f8
sonny <dhsmf4245@gmail.com>
2024-02-01 12:15:51 +0900

    algorism 1463

--- src/main/java/com/example/demo/codingtest/Main.java
+++ src/main/java/com/example/demo/codingtest/Main.java
@@ -1,33 +1,20 @@
 package com.example.demo.codingtest;
 
+
 import java.util.Scanner;
 
-
 public class Main {
-    static Integer[] dp;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        int n = sc.nextInt();
 
-        dp = new Integer[N+1];
-        dp[0] = dp[1] = 0;
-        System.out.println(recur(N));
-    }
-    static int recur(int N){
+        int[] arr = new int[1001];
+        arr[1]=1;
+        arr[2]=2;
 
-        if (dp[N]==null){
-            if (N%6==0){
-                dp[N] = Math.min(recur(N-1), Math.min(recur(N/3), recur(N/2))) + 1;
-            } else if (N%3==0) {
-                dp[N] = Math.min(recur(N/3), recur(N-1))+1;
-            } else if (N%2==0) {
-                dp[N] = Math.min(recur(N/2), recur(N-1))+1;
-            }
-            else {
-                dp[N] = recur(N-1) +1;
-            }
+        for (int i = 3; i <= n; i++) {
+                arr[i] = (arr[i-1] + arr[i-2]) % 10007;
         }
-        return dp[N];
+        System.out.println(arr[n]);
     }
 }
