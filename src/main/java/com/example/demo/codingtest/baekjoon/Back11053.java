40acfe729877c42d74b8761fcf649ef59d4bdda8
sonny <dhsmf4245@gmail.com>
2024-02-09 14:42:59 +0900

    algorism 11053

---
+++ src/main/java/com/example/demo/codingtest/baekjoon/Back11053.java
@@ -0,0 +1,38 @@
+package com.example.demo.codingtest.baekjoon;
+
+import java.util.*;
+public class Main{
+    static int[] seq;
+    static Integer[] dp;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        seq = new int[n];
+        dp = new Integer[n];
+
+        for (int i = 0; i < n; i++) {
+            seq[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            LIS(i);
+        }
+
+        int max = dp[0];
+        for (int i = 1; i < n; i++) {
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+    }
+    static int LIS(int n){
+        if (dp[n]==null){
+            dp[n] = 1;
+            for (int i = n-1; i >= 0; i--) {
+                if (seq[i]<seq[n]){
+                    dp[n] = Math.max(dp[n], LIS(i)+1);
+                }
+            }
+        }
+        return dp[n];
+    }
+}
