db43c198645fd0e65356775dac877a49d44d0372
sonny <dhsmf4245@gmail.com>
2024-02-09 14:44:16 +0900

    revise title

--- src/main/java/com/example/demo/codingtest/baekjoon/Back11053.java
+++ src/main/java/com/example/demo/codingtest/baekjoon/Back11053.java
@@ -1,7 +1,7 @@
 package com.example.demo.codingtest.baekjoon;
 
 import java.util.*;
-public class Main{
+public class Back11053{
     static int[] seq;
     static Integer[] dp;
     public static void main(String[] args) {
