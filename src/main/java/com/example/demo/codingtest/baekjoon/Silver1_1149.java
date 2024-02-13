ca27ff17afe16515aff0ede0981432cd95f9836a
sonny <dhsmf4245@gmail.com>
2024-02-13 13:40:40 +0900

    algorism 1149

--- src/main/java/com/example/demo/codingtest/baekjoon/Silver1_1149.java
+++ src/main/java/com/example/demo/codingtest/baekjoon/Silver1_1149.java
@@ -3,55 +3,50 @@ package com.example.demo.codingtest.baekjoon;
 import java.util.Scanner;
 
 public class Silver1_1149 {
-    final static int Red = 0;
-    final static int Green = 1;
-    final static int Blue = 2;
-
-    static int[][] Cost;
-    static int[][] DP;
-
+    static int dp[];
     public static void main(String[] args) {
+        /*
+        * 배열 어디다 담아둬야 할 듯하다
+        * dp[] 첫번째 빨강 두번째 파랑, 초록 세번째 빨강 초록, 파랑 셋 중에 미니멈
+        * 첫번째 작은 수 n-1번째에 1번이었으면 2, 3 중에 작은 숫자 고르기
+        * 카운트라는 것을 둬야 겄다
+        * if 현재 count가 1이라면
+        * dp[n] = Math.min(dp[n-1]+arr[2],dp[n-1]+arr[3])
+        *  * if 현재 count가 2이라면
+         * dp[n] = Math.min(dp[n-1]+arr[1],dp[n-1]+arr[3])
+         *  *  * if 현재 count가 3이라면
+         * dp[n] = Math.min(dp[n-1]+arr[1],dp[n-1]+arr[2])
+         * 첫번째 두번째가 문제다
+         * 1 5 10
+         * 10 100 200
+         * */
+        int[] arr1 = new int[3];
+        int[] arr2= new int[3];
+        int[] input = new int[3];
+        int count = 0;
+        dp = new  int[1001];
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 3; j++) {
+                input[j] = sc.nextInt();
+                if (i==0){
+                    arr1[j] = input[j];
+                }
+                if (i==1){
+                    arr2[j] = input[j];
+                    //작은 값을 구해야 한다 1 23/ 2 13/ 3 12
+                    //math.min 값을 하면 답을 못 구한다.
 
-        Scanner in = new Scanner(System.in);
-
-        int N = in.nextInt();
-
-        Cost = new int[N][3];
-        DP = new int[N][3];
-
-        for(int i = 0; i < N; i++) {
-            Cost[i][Red] = in.nextInt();
-            Cost[i][Green] = in.nextInt();
-            Cost[i][Blue] = in.nextInt();
+                }
+            }
+            if(count==0){
+                dp[i] = Math.min(dp[i-1]+input[1],dp[i-1]+input[2]);
+            }else if (count==1) {
+                dp[i] = Math.min(dp[i-1]+input[0],dp[i-1]+input[2]);
+            }else if (count==2) {
+                dp[i] = Math.min(dp[i-1]+input[0],dp[i-1]+input[1]);
+            }
         }
-
-        // DP의 첫번째 값(집)은 각 색상비용의 첫번째 값으로 초기화
-        DP[0][Red] = Cost[0][Red];
-        DP[0][Green] = Cost[0][Green];
-        DP[0][Blue] = Cost[0][Blue];
-
-
-        System.out.print(Math.min(Paint_cost(N- 1, Red), Math.min(Paint_cost(N - 1, Green), Paint_cost(N - 1, Blue))));
-    }
-
-    static int Paint_cost(int N, int color) {
-
-        // 만약 탐색하지 않은 배열이라면
-        if(DP[N][color] == 0) {
-
-            // color의 색에 따라 이전 집의 서로 다른 색을 재귀호출하여 최솟값과 현재 집의 비용을 더해서 DP에 저장한다
-            if(color == Red) {
-                DP[N][Red] = Math.min(Paint_cost(N - 1, Green), Paint_cost(N - 1, Blue)) + Cost[N][Red];
-            }
-            else if(color == Green) {
-                DP[N][Green] = Math.min(Paint_cost(N - 1, Red), Paint_cost(N - 1, Blue)) + Cost[N][Green];
-            }
-            else {
-                DP[N][Blue] = Math.min(Paint_cost(N - 1, Red), Paint_cost(N - 1, Green)) + Cost[N][Blue];
-            }
-
-        }
-
-        return DP[N][color];
     }
 }
