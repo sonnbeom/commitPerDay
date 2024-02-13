e208269767137a4b37477ebf287c58f7e5c1304c
sonny <dhsmf4245@gmail.com>
2024-02-13 16:56:08 +0900

    algorithm 16953

---
+++ src/main/java/com/example/demo/codingtest/baekjoon/Silver2_16953.java
@@ -0,0 +1,38 @@
+package com.example.demo.codingtest.baekjoon;
+
+import java.util.*;
+public class Main {
+    static int count;
+    static int impossible = -1;
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b= sc.nextInt();
+
+        int result = minCount(a,b);
+        if(result != impossible){
+            System.out.println(count+1);
+        }else{
+            System.out.println(impossible);
+        }
+    }
+    static int minCount(int start, int result) {
+        while(result > start){
+            if ((result - 1) % 10 == 0) {
+                result = (result - 1) / 10;
+                count ++;
+            }else if (result % 2 == 0) {
+                count ++;
+                result = result / 2;
+            }else{
+                return impossible;
+            }
+        }
+        if(start == result){
+            return count+1;
+        }else{
+            return impossible;
+        }
+    }
+}
+
---
+++ src/main/java/com/example/demo/codingtest/baekjoon/Silver4_2217.java
@@ -0,0 +1,32 @@
+package com.example.demo.codingtest.baekjoon;
+
+import java.util.*;
+public class Main {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Integer arr[] = new Integer[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr, Collections.reverseOrder());
+        int max = 0;
+        for(int i = 0; i < n; i++){
+            max = Math.max(max, arr[i]*(i+1));
+        }
+        System.out.println(max);
+
+
+        /*
+        * 10 20 30 40 50
+        *
+        *
+        * 50 40 30 20 10
+        * 50
+        * 80
+        * 90
+        * 80
+        *
+        * */
+    }
+}
