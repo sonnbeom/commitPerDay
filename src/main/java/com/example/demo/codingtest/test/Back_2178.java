f1c68f38211d4f58c09bb1551a7376e0c8308c79
sonny <dhsmf4245@gmail.com>
2024-02-16 09:12:00 +0900

    algorithm 2178

--- src/main/java/com/example/demo/codingtest/test/Back_2178.java
+++ src/main/java/com/example/demo/codingtest/test/Back_2178.java
@@ -3,13 +3,13 @@ package com.example.demo.codingtest.test;
 
 
 import java.util.*;
-public class Back_2178 {
+public class Main {
     static int N;
     static int M;
     static boolean[][] visit;
     static int[][] miro;
-    static int[] x = {0,0,-1,1};
-    static int[] y = {-1,1,0,0};
+    static int[] ax = {0,0,-1,1};
+    static int[] ay = {-1,1,0,0};
     //좌우상하
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
@@ -19,15 +19,39 @@ public class Back_2178 {
         visit = new boolean[N][M];
         for(int i =0; i<N; i++){
             String input = sc.next();
-            for(int j =0; i<M; j++){
+            for(int j =0; j<M; j++){
                 miro[i][j] = input.charAt(j) - '0';
             }
         }
         bfs();
+        System.out.println(miro[N-1][M-1]);
     }
     static void bfs(){
-        Queue queue = new LinkedList<int[]>();
+        visit[0][0] = true;
+        int y,x = 0;
+        int nowX, nowY = 0;
+        Queue<int[]> queue = new LinkedList<int[]>();
+        int[] first = {0,0};
+        queue.add(first);
+        int[] tmp = new int[2];
 
+        while(!queue.isEmpty()){
+            tmp = queue.poll();
+            x = tmp[1];
+            y = tmp[0];
+            for(int i =0; i<4; i++){
+                nowX = x + ax[i];
+                nowY = y + ay[i];
+                if(check(nowX, nowY) && miro[nowY][nowX]==1 && visit[nowY][nowX] == false){
+                    visit[nowY][nowX] = true;
+                    miro[nowY][nowX] += miro[y][x];
+                    queue.add(new int[] {nowY, nowX});
+                }
+            }
+        }
 
+    }
+    static boolean check(int nowX, int nowY){
+        return (nowX>=0 && nowX<M && nowY>=0 && nowY<N);
     }
 }
