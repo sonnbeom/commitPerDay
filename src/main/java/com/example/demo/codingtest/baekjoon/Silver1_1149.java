01d5f01f94a056940ab9d0ff88f28b1b55ba8319
sonny <dhsmf4245@gmail.com>
2024-02-16 12:56:34 +0900

    algorithm re

--- src/main/java/com/example/demo/codingtest/baekjoon/Silver1_1149.java
+++ src/main/java/com/example/demo/codingtest/baekjoon/Silver1_1149.java
@@ -1,29 +1,29 @@
 package com.example.demo.codingtest.baekjoon;
 
 import java.util.*;
-public class Silver1_1149 {
-    final static int Red = 0;
-    final static int Green = 1;
-    final static int Blue = 2;
-
+public class Main {
+    static int RED = 0;
+    static int GREEN = 1;
+    static int BLUE = 2;
+    static int DP[][];
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[][] cost = new int[N][3];
-
+        DP = new int[N][3];
         for(int i = 0; i < N; i++){
-            cost[i][Red] = sc.nextInt();
-            cost[i][Green] = sc.nextInt();
-            cost[i][Blue] = sc.nextInt();
+          DP[i][RED] = sc.nextInt();
+          DP[i][GREEN] = sc.nextInt();
+          DP[i][BLUE] = sc.nextInt();
         }
-        for(int i = 1; i<N; i++){
-            cost[i][Red] = Math.min(cost[i-1][Blue], cost[i-1][Green]) + cost[i][Red];
-            cost[i][Blue] = Math.min(cost[i-1][Red], cost[i-1][Green]) + cost[i][Blue];
-            cost[i][Green] = Math.min(cost[i-1][Blue], cost[i-1][Red]) + cost[i][Green];
+        for(int i =1; i<N; i++){
+            DP[i][RED] = Math.min(DP[i-1][GREEN],DP[i-1][BLUE]) + DP[i][RED];
+            DP[i][BLUE] = Math.min(DP[i-1][GREEN],DP[i-1][RED]) + DP[i][BLUE];
+            DP[i][GREEN] = Math.min(DP[i-1][RED],DP[i-1][BLUE]) + DP[i][GREEN];
         }
-        int min = Math.min(Math.min(cost[N-1][Red], cost[N-1][Blue]), cost[N-1][Green]);
+        int min = Math.min(DP[N-1][GREEN], Math.min(DP[N-1][RED],DP[N-1][BLUE]));
         System.out.println(min);
     }
 }
+
 
 
--- src/main/java/com/example/demo/codingtest/test/Back_1789.java
+++ src/main/java/com/example/demo/codingtest/test/Back_1789.java
@@ -1,25 +1,20 @@
 package com.example.demo.codingtest.test;
 
 
-
-
-
-
-
 import java.util.*;
 public class Main {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         long S = sc.nextLong();
-        long sum = 0L;
-        int N = 0;
-
-        for(int i =0; ; i++){
+        long sum = 0;
+        int count = 0;
+        for(int i=1; ;i++){
             sum += i;
+            count = i;
             if(sum>S){
-                N=i-1;
                 break;
             }
-        }System.out.println(N);
+        }
+        System.out.println(count-1);
     }
 }
